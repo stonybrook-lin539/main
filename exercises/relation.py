85e31ff335172fcb3b06a8b724c9c645b0f7dd80
Kenneth Hanson <khanson679@gmail.com>
2021-07-30 17:45:55 -0400

    Class for mathematical relation.

---
+++ exercises/relation.py
@@ -0,0 +1,63 @@
+"""
+Class representing a mathematical relation.
+"""
+
+
+class Relation:
+
+    def __init__(self, domain, mapping):
+        """
+        domain - set of elements
+        mapping - set of pairs of elements in the domain
+        """
+        if (not all(x in domain and y in domain for x, y in mapping)):
+            raise ValueError("Mapping contains element not in domain")
+        self.domain = domain
+        self.mapping = mapping
+
+    def __str__(self):
+        return ("<Relation>"
+                f"\nDomain: {self.domain}"
+                f"\nMapping: {self.mapping}")
+
+    def is_reflexive(self):
+        return all((y, x) in self.mapping for (x, y) in self.mapping)
+
+    def is_irreflexive(self):
+        return not self.is_reflexive()
+
+    def is_transitive(self):
+        return all((a, d) in self.mapping
+                   for (a, b) in self.mapping
+                   for (c, d) in self.mapping
+                   if b == c)
+
+    def is_symmetric(self):
+        return all((y, x) in self.mapping for (x, y) in self.mapping)
+
+    def is_antisymmetric(self):
+        return not any((y, x) in self.mapping for (x, y) in self.mapping
+                       if x != y)
+
+    def is_asymmetric(self):
+        return not any((y, x) in self.mapping for (x, y) in self.mapping)
+
+
+if __name__ == "__main__":
+    r1 = Relation(set("abcd"), [("a","a"), ("b","b"), ("c","c")])
+    r2 = Relation(set("abcd"), [("a","a"), ("b","b"), ("c","d")])
+    r3 = Relation(set("abcd"), [("a","b"), ("b","c"), ("c","d"),
+                                ("a","c"), ("b","d"), ("a","d")])
+    r4 = Relation(set("abcd"), [("a","b"), ("b","a"), ("c","d"), ("d","c")])
+    r5 = Relation(set("abcd"), [("a","b"), ("b","a"), ("c","d"), ("d","d")])
+
+    for i, r in enumerate((r1, r2, r3, r4, r5)):
+        print(i + 1)
+        print(r)
+        print("reflexive", r.is_reflexive())
+        print("irreflexive", r.is_irreflexive())
+        print("symmetric", r.is_symmetric())
+        print("antisymmetric", r.is_antisymmetric())
+        print("asymmetric", r.is_asymmetric())
+        print()
+
