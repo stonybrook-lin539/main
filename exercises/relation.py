99c2754d78400c61a52051180f09d6480deb62e1
Kenneth Hanson <khanson679@gmail.com>
2021-08-02 16:31:05 -0400

    Add functions to make relation reflexive, transitive, etc.

--- exercises/relation.py
+++ exercises/relation.py
@@ -3,6 +3,7 @@ Class representing a mathematical relation.
 """
 
 import pydot
+from itertools import combinations
 
 class Relation:
 
@@ -52,6 +53,81 @@ class Relation:
     def is_asymmetric(self):
         return not any((y, x) in self.mapping for (x, y) in self.mapping)
 
+    def make_reflexive(self):
+        """Add elements to mapping to make the relation reflexive."""
+        for x in self.domain:
+            if not (x, x) in self.mapping:
+                self.mapping.append((x, x))
+        print(self)
+        assert self.is_reflexive()
+
+    def make_irreflexive(self):
+        """
+        Remove elements from mapping to make the relation irreflexive.
+        """
+        self._eliminate_reflexive_pairs()
+        assert self.is_irreflexive()
+
+    def make_transitive(self):
+        """Add elements to mapping to make the relation transitive."""
+        for (a, b) in self.mapping:
+            for (c, d) in self.mapping:
+                if b == c and not (a, d) in self.mapping:
+                    self.mapping.append((a, d))
+        assert self.is_transitive()
+
+    def make_symmetric(self):
+        """Add elements to mapping to make the relation symmetric."""
+        for (x, y) in self.mapping:
+            if not (y, x) in self.mapping:
+                self.mapping.append((y, x))
+        assert self.is_symmetric()
+
+    def make_antisymmetric(self):
+        """
+        Remove elements from mapping to make the relation antisymmetric. When
+        there is a pair of elements (x, y) and (y, x), remove the element that
+        is ordered last by sorting
+        """
+        self._eliminate_symmetric_pairs()
+        assert self.is_antisymmetric()
+
+    def make_asymmetric(self):
+        """
+        Remove elements from mapping to make the relation asymmetric. When
+        there is a pair of elements (x, y) and (y, x), remove the element that
+        is ordered last by sorting.
+        """
+        self._eliminate_reflexive_pairs()
+        self._eliminate_symmetric_pairs()
+        assert self.is_asymmetric()
+
+    def _eliminate_reflexive_pairs(self):
+        """
+        Remove all elements (x, x) in the mapping. Iterate
+        over a copy, since we are removing elements while iterating.
+        """
+        for (x, y) in self.mapping[:]:
+            if x == y:
+                self.mapping.remove((x, x))
+
+    def _eliminate_symmetric_pairs(self):
+        """
+        When there is a pair of elements (x, y) and (y, x) in the mapping,
+        remove the element that is ordered last by sorting.
+
+        The simplest way to do this is to check all combinations of elements,
+        decide which to remove, and then do removal in a separate step.
+        Iterating over a copy doesn't work for this, and creating a new list
+        and replacing the old one can cause strange bugs.
+        """
+        toremove = []
+        for (a, b), (c, d) in combinations(self.mapping, 2):
+            if a == d and b == c:
+                toremove.append((c, d))
+        for (x, y) in toremove:
+            self.mapping.remove((x, y))
+
 
 if __name__ == "__main__":
     r1 = Relation(set("abcd"), [("a","a"), ("b","b"), ("c","c"), ("d", "d")])
@@ -61,13 +137,32 @@ if __name__ == "__main__":
     r4 = Relation(set("abcd"), [("a","b"), ("b","a"), ("c","d"), ("d","c")])
     r5 = Relation(set("abcd"), [("a","b"), ("b","a"), ("c","d"), ("d","d")])
 
-    for i, r in enumerate((r1, r2, r3, r4, r5)):
-        print(i + 1)
-        print(r)
-        print("reflexive", r.is_reflexive())
-        print("irreflexive", r.is_irreflexive())
-        print("symmetric", r.is_symmetric())
-        print("antisymmetric", r.is_antisymmetric())
-        print("asymmetric", r.is_asymmetric())
-        print()
+    # for i, r in enumerate((r1, r2, r3, r4, r5)):
+    #     print(i + 1)
+    #     print(r)
+    #     print("reflexive", r.is_reflexive())
+    #     print("irreflexive", r.is_irreflexive())
+    #     print("symmetric", r.is_symmetric())
+    #     print("antisymmetric", r.is_antisymmetric())
+    #     print("asymmetric", r.is_asymmetric())
+    #     print()
 
+    print(r5)
+
+    # r5.make_reflexive()
+    # print(r5)
+
+    # r5.make_irreflexive()
+    # print(r5)
+
+    # r5.make_transitive()
+    # print(r5)
+
+    # r5.make_symmetric()
+    # print(r5)
+
+    # r5.make_antisymmetric()
+    # print(r5)
+
+    # r5.make_asymmetric()
+    # print(r5)
