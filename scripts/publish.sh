11a4359f57ac9c7c3ce6ad947879f2e278c46b8f
Thomas Graf <git@thomasgraf.net>
2023-09-07 21:15:19 -0400

    make subtree push more resilient

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,4 +7,5 @@ rsync -av --prune-empty-dirs --delete build/html/ output/html
 git add output
 git commit -m "recompile pdf & html"
 git push origin master
-git subtree push --prefix output/html origin gh-pages
+# git subtree push --prefix output/html origin gh-pages
+git push origin `git subtree split --prefix output/html master`:gh-pages --force
