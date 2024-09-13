b726cd8fe7e6c2d70e596ccbe1047df10287febc
Thomas Graf <git@thomasgraf.net>
2024-09-13 13:02:25 -0400

    for convenience: publish.sh runs doit html_website first

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+doit html_website
+
 rsync -av --prune-empty-dirs --delete --include '*/' --include '*.pdf' --exclude '*' build/pdf/sections/ output/pdf/
 
 rsync -av --prune-empty-dirs --delete build/html/ output/html
